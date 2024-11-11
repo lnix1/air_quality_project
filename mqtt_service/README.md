e102b81436f36c8fb852268283ec66eee6440426
lnix1 <louis.nix2@gmail.com>
2024-11-11 11:43:33 -0800

    complete rewrite of producer in server.py to be a wrapper of the java command line producer setup

--- mqtt_service/README.md
+++ mqtt_service/README.md
@@ -3,4 +3,5 @@
 - ssh into EC2 instance
 - scp files from "grp_service" to EC2
 - run "install" bash script
-- run python3 server.py to launch gRPC service
+- run python3 server.py --kafka-servers YOUR_MSK_BROKER_URL_HERE --pub-topic YOUR_KAFKA_TOPIC_HERE --configs PATH_TO_YOUR_CONFIG.PROPERTIES_FILE
+    - For config.properties, this is created during the MSK setup tutorial on AWS (here: https://docs.aws.amazon.com/msk/latest/developerguide/create-topic.html)
--- mqtt_service/server.py
+++ mqtt_service/server.py
@@ -1,30 +1,40 @@
-from concurrent import futures
-from paho.mqtt import client as mqtt_client
-from kafka import KafkaProducer
-import json
+# Basic template taken from: https://github.com/maxcotec/aws-IAM-auth-msk-python/blob/main/main.py
+
+import argparse
 import threading
 from creds import creds_dict
+from paho.mqtt import client as mqtt_client
+
+import subprocess as subp
 
 # Configuration for ThingSpeak's MQTT
-MQTT_BROKER = creds_dict["BROKER"]
-MQTT_PORT = creds_dict["PORT"] 
-MQTT_TOPIC = creds_dict["TOPIC"] 
-MQTT_CLIENT = creds_dict["CLIENT"]
-MQTT_USERNAME = creds_dict["USERNAME"]
-MQTT_PASSWORD = creds_dict["PASSWORD"]
+MQTT_BROKER = creds_dict["MQTT_BROKER"]
+MQTT_PORT = creds_dict["MQTT_PORT"] 
+MQTT_TOPIC = creds_dict["MQTT_TOPIC"] 
+MQTT_CLIENT = creds_dict["MQTT_CLIENT"]
+MQTT_USERNAME = creds_dict["MQTT_USERNAME"]
+MQTT_PASSWORD = creds_dict["MQTT_PASSWORD"]
 
-# Configuration for Kafka (AWS MSK)
-KAFKA_BROKER_URL = "b-1.democluster1.iv249r.c3.kafka.us-west-1.amazonaws.com:9098"  # Replace with your MSK cluster's broker URL
-KAFKA_TOPIC = "DustSensorTopic"
+def parse_arguments():
+    """ parse the arguments of the script """
+    parser = argparse.ArgumentParser(description='Kafka Sub Pub')
 
-# Initialize Kafka producer
-producer = KafkaProducer(
-    bootstrap_servers=[KAFKA_BROKER_URL],
-    value_serializer=lambda v: json.dumps(v).encode('utf-8'),
-    security_protocol="SSL"
-)
+    # Required Arguments
+    required_parser = parser.add_argument_group(title='required arguments')
 
-def connect_mqtt() -> mqtt_client:
+    required_parser.add_argument('--kafka-servers', help="kafka servers (comma seperated)")
+    required_parser.add_argument('--pub-topic', required=True, help="topic where to publish data to")
+    required_parser.add_argument('--configs', required=False,
+                                 help="custom client properties to enable IAM auth enabled kafka cluster.")
+
+    # optional
+    parser.add_argument('--kafka-path', default="/home/ec2-user/kafka_2.13-3.6.0",
+                        help="location where kafka is installed")
+    parser.add_argument('--aws-region', default="us-west-1", help="aws region")
+    return parser.parse_args()
+
+
+def create_cli_consumer():
     def on_connect(client, userdata, flags, rc, props=None):
         if rc == 0:
             print("Connected to MQTT Broker!")
@@ -37,21 +47,69 @@ def connect_mqtt() -> mqtt_client:
     client.connect(MQTT_BROKER, MQTT_PORT)
     return client
 
+def create_cli_producer(arguments):
+    print(f"Initializing kafka producer for servers: {arguments.kafka_servers}")
+    print(f"topic: {arguments.pub_topic}")
 
-def subscribe(client: mqtt_client):
+    kafka_producer_init_cmd = [
+        f"{arguments.kafka_path}/bin/kafka-console-producer.sh",
+        "--topic", arguments.pub_topic,
+        "--bootstrap-server", arguments.kafka_servers
+    ]
+
+    if arguments.configs:
+        kafka_producer_init_cmd = kafka_producer_init_cmd + ["--producer.config", arguments.configs]
+
+    try:
+        proc = subp.Popen(kafka_producer_init_cmd, stdin=subp.PIPE)
+        print("kafka producer init done.")
+        return proc
+    except Exception as e:
+        print(f"Error creating producer: {e}")
+        return None
+
+
+# Define a function to consume messages
+def consume_messages(consumer, producer):
     def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        message = msg.payload.decode()
+        send_msg_thread = threading.Thread(target=send_message, args=(producer, message))
+        send_msg_thread.start()
 
-    client.subscribe(MQTT_TOPIC)
-    client.on_message = on_message
+    consumer.subscribe(MQTT_TOPIC)
+    consumer.on_message = on_message
+    consumer.loop_start()
+
+def send_message(producer, msg):
+    # Publish the received message to the producer
+    try:
+        print(f"Publishing message: {msg}")
+        producer.stdin.write(msg.encode() + b"\n")
+        producer.stdin.flush()
+    except Exception as e:
+        print(f"Error sending message: {e}")
 
 
-def run():
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
+def main():
+    args = parse_arguments()
+
+    # Create the producer process in a separate thread
+    kafka_producer = create_cli_producer(args)
+
+    # Create the consumer process
+    kafka_consumer = create_cli_consumer()
+
+    # Start the Kafka consumer thread
+    consumer_thread = threading.Thread(target=consume_messages, args=(kafka_consumer, kafka_producer))
+    consumer_thread.daemon = True
+    consumer_thread.start()
+
+    # Your main program logic can continue here while the consumer and producer threads are running
+
+    # For example, you can add a loop to keep the main thread alive or perform other operations.
+    while True:
+        pass
 
 
-if __name__ == '__main__':
-    run()
-
+if __name__ == "__main__":
+    main()
